efeb4258cce23e021e80206b116606c42271fe79
Akhmad Roviansyah <roviearo@gmail.com>
2023-03-20 16:30:32 +0700

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,10 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react"
 
 import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -21,9 +25,17 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+
   return (
     <>
       <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList data={movies} title="Trending Now" />
+        <MovieList data={movies} title="My List" />
+      </div>
     </>
   )
 }
